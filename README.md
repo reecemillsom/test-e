6daa0be4334eacc8e8d41bace3c1c18becee8953
reecemillsom <reecemillsom@gmail.com>
2024-06-07 10:27:09 +0100

    1. Writing logic to seed the database with 1 default Battery.
    2. Adding instructions on how to prepare your postgres database.
    
    3. Removing seeding TODO from TODOs file.

--- README.md
+++ README.md
@@ -8,7 +8,10 @@ Build an application that handles battery states.
 
 ## Getting started
 
-
+1. `yarn` - install dependencies in the route of the project
+2. `cd ./api/ && yarn start:db` - This will start the container for the postgres database.
+3. `yarn run:migration:db` - This will run the migration script to instantiate the Battery table
+4. `yarn seed:db` - This will create a default battery in the table to use within the application.
 
 ## Commands
 
--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -1,1 +1,24 @@
-// TODO initialise DB with battery info once schema is decided upon
+import { Battery, PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function run(): Promise<Battery> {
+  const battery = prisma.battery;
+
+  return battery.create({
+    data: {
+      name: 'Existing Battery',
+    },
+  });
+}
+
+run()
+  .then(async () => {
+    console.log('Successfully seeded database>>');
+    await prisma.$disconnect();
+  })
+  .catch(async (error: Error) => {
+    console.error(error);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
